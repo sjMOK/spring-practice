c58371664326035ba8468eb9c078e31a33f4f137
sjMOK <mokseungju@naver.com>
2023-03-14 14:51:25 +0900

    sprint-core #5: singleton container

--- core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -1,6 +1,7 @@
 package hello.core.beanfind;
 
 import hello.core.AppConfig;
+import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
